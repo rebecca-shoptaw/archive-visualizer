c5e799af7831647fe287a7e5459a90712d847f96
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-08-01 15:26:02 -0400

    Set up React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './globals.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./globals.css";
+import ErrorPage from "./components/ErrorPage.tsx";
+import Visualizer from "./components/Visualizer.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/archive-visualizer/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/archive-visualizer/:id",
+    element: <Visualizer />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
