c77adb89451dd5485278420e91c395afb16493d2
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-08-02 17:36:53 -0400

    Fetch related works

--- src/hooks/useFetchedData.tsx
+++ src/hooks/useFetchedData.tsx
@@ -4,6 +4,7 @@ import { MetadataObject } from "../types";
 const useFetchedData = (id: string | undefined) => {
   const [error, setError] = useState(false);
   const [metadata, setMetadata] = useState<null | MetadataObject>(null);
+  const [relatedWorks, setRelatedWorks] = useState<null | []>(null);
   const [filesCount, setFilesCount] = useState(1);
 
   useEffect(() => {
@@ -28,9 +29,22 @@ const useFetchedData = (id: string | undefined) => {
         console.log(err);
         setError(true);
       });
+
+    fetch(`https://be-api.us.archive.org/mds/v1/get_related/all//${id}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.hits.hits) {
+          setRelatedWorks(data.hits.hits);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, [id]);
 
-  return { error, metadata, filesCount };
+  return { error, metadata, filesCount, relatedWorks };
 };
 
 export default useFetchedData;
