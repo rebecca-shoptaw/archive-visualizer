0f6bea63f377542ba8603760390ad90317684d14
Rebecca Shoptaw <rebecca.shoptaw@gmail.com>
2024-08-05 14:33:37 -0400

    Add audio player function comment

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,6 +1,13 @@
 import { AudioPlayerProps } from "../types";
 import styles from "./AudioPlayer.module.css";
 
+/**
+ * Renders an audio player and associated image for the work.
+ * Defaults to rendering it in playlist view.
+ * 
+ * @param props IA identifier for work, work title (for alt text)
+ * @returns Audio player component with work image and playlist iframe
+ */
 const AudioPlayer = (props: AudioPlayerProps) => {
   const { contentId, contentTitle } = props;
   const contentUrl = `https://archive.org/embed/${contentId}&playlist=1`;
